5b28c1b770c78e776a7635f932f6ec25072acbd7
Sidd Srinivasan <siddhartha.srinivasan@gmail.com>
2017-12-13 08:38:32 -0800

    added functions file to handle all major processing. lyrical analysis file will serve as the script

---
+++ lyricanalysis.py
@@ -0,0 +1,38 @@
+import csv
+import nltk
+filename = "songdata.csv"
+
+## Script
+data = []
+
+with open(filename) as csvfile:
+	reader = csv.DictReader(csvfile)
+	for row in reader:
+		data.append(row)
+
+
+#contains unique set of artists
+list_of_artists = []
+for entry in data:
+	artist = entry["artist"]
+	if artist not in list_of_artists:
+		list_of_artists.append(artist)
+
+name = input("Input an Artists name to prompt artifical sentence generation\n")
+
+# Ensure user has valid input.
+while name not in list_of_artists:
+	name = input("Unfortunately we don't have songs on record for this artist, please input another artist \n")
+
+text = []
+for entry in data:
+	if entry["artist"] == artist:
+		text.append(entry["text"])
+
+print text
+
+
+
+
+
+
